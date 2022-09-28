49ee835f48ddd12d6d72d3396d2f1ab96d328ec7
MartineauAdeline <adeline.martineau@lrav.fr>
2022-09-28 14:56:49 +0200

    created the sign-up, form-input and button components

--- src/utils/firebase/firebase.utils.jsx
+++ src/utils/firebase/firebase.utils.jsx
@@ -1,79 +1,69 @@
-// initializeApp : fonction qui fournit une instance de l'application firebase
 import { initializeApp } from 'firebase/app';
-import { 
-  getAuth, 
-  signInWithRedirect, 
-  signInWithPopup, 
-  GoogleAuthProvider 
-} from 'firebase/auth';
-
 import {
-  getFirestore,
-  doc, 
-  getDoc, 
-  setDoc
-} from 'firebase/firestore';
+  getAuth,
+  signInWithRedirect,
+  signInWithPopup,
+  GoogleAuthProvider,
+  createUserWithEmailAndPassword,
+} from 'firebase/auth';
+import { getFirestore, doc, getDoc, setDoc } from 'firebase/firestore';
 
-
-//configuration d'un objet qui permet de rattaché l'instance d'application Firebase à l'instance webapp que l'on a en ligne
-
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyBoDAdtkJo_E3WnLBd5CEvAL0Aq3Cn4LPU",
   authDomain: "crwn-clothing-db-20170.firebaseapp.com",
   projectId: "crwn-clothing-db-20170",
   storageBucket: "crwn-clothing-db-20170.appspot.com",
   messagingSenderId: "415249423626",
-  appId: "1:415249423626:web:548c19a6798c055cdfaef3"
+  appId: "1:415249423626:web:548c19a6798c055cdfaef3",
 };
 
-// Initialize Firebase
 const firebaseApp = initializeApp(firebaseConfig);
 
 const googleProvider = new GoogleAuthProvider();
 
 googleProvider.setCustomParameters({
-  prompt: "select_account"
+  prompt: 'select_account',
 });
 
 export const auth = getAuth();
-export const signInWithGooglePopup = () => signInWithPopup(auth, googleProvider);
-export const signInWithGoogleRedirect = () => signInWithRedirect(auth, googleProvider);
+export const signInWithGooglePopup = () =>
+  signInWithPopup(auth, googleProvider);
+export const signInWithGoogleRedirect = () =>
+  signInWithRedirect(auth, googleProvider);
 
 export const db = getFirestore();
 
-export const createUserDocumentFromAuth = async (userAuth) => {
+export const createUserDocumentFromAuth = async (
+  userAuth,
+  additionalInformation = {}
+) => {
+  if (!userAuth) return;
+
   const userDocRef = doc(db, 'users', userAuth.uid);
 
-  console.log(userDocRef)
-
-  //permet de vérifier si il existe une instance user dans la bdd
   const userSnapshot = await getDoc(userDocRef);
-  console.log(userSnapshot);
-  console.log(userSnapshot.exists());
 
-  if(!userSnapshot.exists()) {
+  if (!userSnapshot.exists()) {
     const { displayName, email } = userAuth;
-    const createdAt = new Date();  
+    const createdAt = new Date();
 
     try {
       await setDoc(userDocRef, {
-        displayName, 
-        email, 
-        createdAt
+        displayName,
+        email,
+        createdAt,
+        ...additionalInformation,
       });
-    } catch(error) {
+    } catch (error) {
       console.log('error creating the user', error.message);
     }
   }
 
   return userDocRef;
+};
 
+export const createAuthUserWithEmailAndPassword = async (email, password) => {
+  if (!email || !password) return;
 
-  //if userdata exists
-  
-  //if userdata does not exists
-  
-  //return userDocRef
-
+  return await createUserWithEmailAndPassword(auth, email, password);
 };
